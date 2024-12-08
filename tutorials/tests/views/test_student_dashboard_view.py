d6dbb01d886ba1c0c48e42cb18bc80fe3b9db9a8
RapidShotzz <arjan.bedi@kcl.ac.uk>
2024-12-08 18:15:37 +0000

    added testcase to ensure the student dashboard appears when a user logs in

---
+++ tutorials/tests/views/test_student_dashboard_view.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+class StudentDashboardViewTestCase(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(username='testuser', password='password')
+
+    def test_student_dashboard_view_authenticated(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.get(reverse('student_dashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'student_dashboard_templates/student_dashboard_in.html')
+        self.assertIn('user', response.context)
+        self.assertEqual(response.context['user'], 'Student!')
+
