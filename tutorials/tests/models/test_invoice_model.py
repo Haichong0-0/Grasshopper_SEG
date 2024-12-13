a18884d686e09ca488390c1def20980df7a94019
RapidShotzz <arjan.bedi@kcl.ac.uk>
2024-12-13 14:52:26 +0000

    got rid of invoice error

--- tutorials/tests/models/test_invoice_model.py
+++ tutorials/tests/models/test_invoice_model.py
@@ -1,30 +1,52 @@
 from django.test import TestCase
-from tutorials.models import Invoice
-
+from tutorials.models import Invoice, Student, Tutor
 
 class InvoiceModelTestCase(TestCase):
     def setUp(self):
 
+        self.student = Student.objects.create(
+            username="rapidstudent",
+            email="test@student.com",
+            password="password123"
+        )
+
+        self.tutor = Tutor.objects.create(
+            username="rapidtutor",
+            email="rapid@tutor.com",
+            password="password1234"
+        )
+
+
         self.invoice = Invoice.objects.create(
-            no_of_classes=5,
+            student=self.student,
+            tutor=self.tutor,
+            no_of_classes=10,
             price_per_class=50.00
         )
 
-    def test_invoice_creation(self):
-        self.assertEqual(self.invoice.no_of_classes, 5)
-        self.assertEqual(self.invoice.price_per_class, 50.00)
-        self.assertEqual(self.invoice.totalsum, 0)
 
-    def test_calc_sum(self):
-        self.invoice.calc_sum()
-        self.assertEqual(self.invoice.totalsum, 250.00)
+        self.invoice.total_sum = self.invoice.no_of_classes * self.invoice.price_per_class
+        self.invoice.save()
+
+    def test_invoice_creation(self):
+        self.assertEqual(self.invoice.no_of_classes, 10)
+        self.assertEqual(self.invoice.price_per_class, 50.00)
+        expected_total_sum = self.invoice.no_of_classes * self.invoice.price_per_class
+        self.assertEqual(self.invoice.total_sum, expected_total_sum)
 
     def test_invoice_with_custom_totalsum(self):
-        self.invoice.totalsum = 300.00
-        self.assertEqual(self.invoice.totalsum, 300.00)
+
+        self.invoice.total_sum = 300.00
+        self.invoice.save()
+        self.assertEqual(self.invoice.total_sum, 300.00)
 
     def test_invoice_sum_on_update(self):
+
         self.invoice.no_of_classes = 10
         self.invoice.price_per_class = 50.00
-        self.invoice.calc_sum()
-        self.assertEqual(self.invoice.totalsum, 500.00)
+        self.invoice.save()
+
+        expected_total_sum = self.invoice.no_of_classes * self.invoice.price_per_class
+        self.assertEqual(self.invoice.total_sum, expected_total_sum)
+
+
