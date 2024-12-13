4857c56577fdc9bdfaad87693c677689f54702ff
RapidShotzz <arjan.bedi@kcl.ac.uk>
2024-12-13 15:19:31 +0000

    fixed errors with subject model

--- tutorials/tests/models/test_subjects_model.py
+++ tutorials/tests/models/test_subjects_model.py
@@ -1,57 +1,81 @@
-from django.core.exceptions import ValidationError
-from django.db.utils import IntegrityError  # Import the correct exception
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from tutorials.models import Subjects
+from tutorials.models import Subjects, Tutor
 
-class SubjectsModelTest(TestCase):
+
+class SubjectModelTest(TestCase):
 
     def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            username="rapiduser",
-            password="password123",
-            email="rapid@example.com"
+        self.user1 = get_user_model().objects.create_user(
+            username='testuser1', password='testpassword', email='testuser1@gmail.com'
+        )
+        self.user2 = get_user_model().objects.create_user(
+            username='testuser2', password='testpassword', email='testuser2@gmail.com'
         )
 
-    def test_subject_creation(self):
+    def test_create_subject(self):
         subject = Subjects.objects.create(
-            user=self.user,
-            subject='python',
-            proficiency=Subjects.INTERMEDIATE
+            subject_name='Python',
+            timings='Mon, Wed, Fri - 10 AM to 12 PM',
+            bio='A high-level programming language for general-purpose programming.'
         )
-        self.assertEqual(subject.user, self.user)
-        self.assertEqual(subject.subject, 'python')
-        self.assertEqual(subject.proficiency, Subjects.INTERMEDIATE)
 
-    def test_subject_default_proficiency(self):
+        self.assertEqual(subject.subject_name, 'Python')
+        self.assertEqual(subject.timings, 'Mon, Wed, Fri - 10 AM to 12 PM')
+        self.assertEqual(subject.bio, 'A high-level programming language for general-purpose programming.')
+
+    def test_subject_name_uniqueness(self):
+        subject1 = Subjects.objects.create(
+            subject_name='Ruby on Rails',
+            timings='Mon, Wed - 2 PM to 4 PM'
+        )
+
+        with self.assertRaises(Exception):
+            subject2 = Subjects.objects.create(
+                subject_name='Ruby on Rails',
+                timings='Tue, Thu - 3 PM to 5 PM'
+            )
+
+    def test_default_tutor_list(self):
+        subject = Subjects.objects.create(subject_name='Django')
+        self.assertEqual(subject.tutor_list, [])
+
+    def test_tutor_association(self):
+        subject = Subjects.objects.create(subject_name='JavaScript')
+
+        tutor = Tutor.objects.create_user(
+            username='tutor1', password='password123', email='tutor1@example.com'
+        )
+        tutor.subjects.add(subject)
+
+        self.assertIn(subject, tutor.subjects.all())
+        self.assertEqual(subject.tutors.count(), 1)
+        self.assertEqual(subject.tutors.first(), tutor)
+
+    def test_tutor_list_on_subject_after_association(self):
+        subject = Subjects.objects.create(subject_name='React')
+
+        tutor1 = Tutor.objects.create_user(
+            username='tutor1', password='password123', email='tutor1@example.com'
+        )
+        tutor2 = Tutor.objects.create_user(
+            username='tutor2', password='password123', email='tutor2@example.com'
+        )
+
+        subject.tutor_list.extend([tutor1.id, tutor2.id])
+        subject.save()
+        subject.refresh_from_db()
+        self.assertIn(tutor1.id, subject.tutor_list)
+        self.assertIn(tutor2.id, subject.tutor_list)
+
+    def test_subject_with_optional_fields(self):
         subject = Subjects.objects.create(
-            user=self.user,
-            subject='ruby_on_rails'
+            subject_name='Node.js',
+            timings='Tue, Thu - 1 PM to 3 PM',
+            bio=None
         )
 
-        self.assertEqual(subject.proficiency, Subjects.INTERMEDIATE)
+        self.assertIsNone(subject.bio)
+        self.assertEqual(subject.timings, 'Tue, Thu - 1 PM to 3 PM')
 
-    def test_invalid_subject(self):
-        with self.assertRaises(ValidationError):
-            subject = Subjects(
-                user=self.user,
-                subject='Invalid_Subject',
-                proficiency=Subjects.BEGINNER
-            )
-            subject.full_clean()
 
-    def test_invalid_proficiency(self):
-        with self.assertRaises(ValidationError):
-            subject = Subjects(
-                user=self.user,
-                subject='Python',
-                proficiency='invalid_proficiency'
-            )
-            subject.full_clean()
-
-    def test_subject_foreign_key(self):
-        with self.assertRaises(IntegrityError):
-            Subjects.objects.create(
-                subject='Javascript',
-                proficiency=Subjects.MASTERY
-            )
